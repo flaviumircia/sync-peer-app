62018c6a50fa77305f2741f6c8aa692c600cd658
Flaviu <flaviumircia15@gmail.com>
2024-01-21 21:48:38 +0200

    Feature: Add select user feature

--- app/src/main/java/com/syncpeer/syncpeerapp/videocall/HomeActivity.kt
+++ app/src/main/java/com/syncpeer/syncpeerapp/videocall/HomeActivity.kt
@@ -1,99 +1,113 @@
 package com.syncpeer.syncpeerapp.videocall
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.syncpeer.syncpeerapp.R
 import com.syncpeer.syncpeerapp.auth.utils.Constants
-import com.syncpeer.syncpeerapp.videocall.webrtc.PeerToPeerConnectionEstablishment
-import com.syncpeer.syncpeerapp.videocall.webrtc.mediators.DestinationEmailMediator
-import java.util.concurrent.Executors
-import java.util.concurrent.ScheduledExecutorService
-import java.util.concurrent.TimeUnit
-
+import com.syncpeer.syncpeerapp.videocall.users.SelectUserDto
+import com.syncpeer.syncpeerapp.videocall.users.UserCard
 
 class HomeActivity : AppCompatActivity() {
-    private var peerToPeerConnectionEstablishment: PeerToPeerConnectionEstablishment? = null
+    private var list: List<SelectUserDto>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         val email = applicationContext
             .getSharedPreferences(Constants.USER_EMAIL, MODE_PRIVATE)
             .getString(Constants.USER_EMAIL, null)
-
-        peerToPeerConnectionEstablishment = PeerToPeerConnectionEstablishment(
-            applicationContext,
-            email,
-            DestinationEmailMediator(),
+         val listOfUsers = listOf(
+            SelectUserDto(
+                0,
+                "test_d@gmail.com",
+                "test",
+                R.drawable.check_icon
+            ),
+            SelectUserDto(
+                1,
+                "test@gmail.com",
+                "test2",
+                R.drawable.check_icon
+            )
         )
-
-        val executor: ScheduledExecutorService = Executors.newScheduledThreadPool(1)
-        val task = Runnable {
-            peerToPeerConnectionEstablishment?.initializePeerConnections()
-        }
-        val future = executor.scheduleAtFixedRate(task,0,5,TimeUnit.SECONDS)
-
-
-    setContent {
-            MainScreen()
+        list = listOfUsers.filter { user -> user.name != email }
+        setContent {
+            SelectUserScreen()
         }
     }
+
     @Composable
     @Preview
-    fun MainScreen() {
-        var messageToSend by remember { mutableStateOf("") }
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            TextField(
-                value = messageToSend,
-                onValueChange = { messageToSend = it },
-                label = { Text("Type your message here") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-            )
-            Button(
-                onClick = {
-                    peerToPeerConnectionEstablishment?.sendMessageToPeer(messageToSend,"test_d@gmail.com")
-                },
-                colors = ButtonDefaults.elevatedButtonColors(
-                    containerColor = Color.White,
-                    contentColor = Color.Red
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        start = 40.dp,
-                        end = 40.dp
-                    )
-                    .height(40.dp),
-            ) {
-                Text(text = "Send Message to Peer test_d@gmail.com!")
+    fun SelectUserScreen() {
+        list.let {
+            if (it != null) {
+                allUsers(
+                    userList = it
+                )
             }
         }
     }
 
+
+    @Composable
+    fun allUsers(userList: List<SelectUserDto>) {
+        Scaffold (  bottomBar = {}){
+            it.calculateBottomPadding()
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                item {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                            .padding(vertical = 25.dp),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    )
+                    {
+                        Text(
+                            "\uD83C\uDF3FCall any user you want!",
+                            style = MaterialTheme.typography.headlineMedium
+                        )
+                    }
+                }
+                items(userList) { user ->
+                    UserCard(
+                        name = user.name,
+                        lastMessage = user.lastMessage,
+                        image = user.imageRes,
+                        onClick = {
+                            val intent = Intent(this@HomeActivity, VideoCallActivity::class.java)
+                            intent.putExtra("destination_mail",user.name)
+                            startActivity(intent)
+                        })
+
+                }
+            }
+        }
+
+    }
 }
